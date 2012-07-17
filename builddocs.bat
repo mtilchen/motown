d2ac2a58be873541d9d062da0423ed85bd7187c8
Matthew P. Tilchen <mtilchen@gmail.com>
2012-07-17 17:55:41 -0400

    Update path to JSDuck

--- builddocs.bat
+++ builddocs.bat
@@ -1,2 +1,2 @@
-C:\Users\Matthew\Downloads\jsduck-3.11.0.exe --head-html='^<link rel="stylesheet" href="resources/css/motown-docs.css" type="text/css"^>' --categories .\doc-src\doc-categories.json --title "Motown" --output .\doc .\motown.js
+jsduck-3.11.0.exe --head-html='^<link rel="stylesheet" href="resources/css/motown-docs.css" type="text/css"^>' --categories .\doc-src\doc-categories.json --title "Motown" --output .\doc .\motown.js
 cp -r .\doc-src\resources .\doc\
