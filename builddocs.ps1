e61a263de9e0f82640687646f0fd837cae9ed875
Matthew P. Tilchen <mtilchen@gmail.com>
2012-07-18 09:53:58 -0400

    Change from batch file to Powershell script for building docs

---
+++ builddocs.ps1
@@ -0,0 +1,4 @@
+& C:\Users\Matthew\Downloads\jsduck-3.11.0.exe --head-html='<link rel=\"stylesheet\" href=\"resources/css/motown-docs.css\" type=\"text/css\">' --categories .\doc-src\doc-categories.json --title "Motown" --output .\doc .\motown.js
+
+cp -r -force .\doc-src\resources .\doc\
+cp -r -force .\doc\* ..\motowndoc
